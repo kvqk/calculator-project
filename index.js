0b201254987a2a2106b52387b3194d3e35555f64
Tyler <tyler@class-computers-MacBook-Air.local>
2022-05-04 18:43:59 -0700

    function

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const numbers = document.getElementsByClassName("numbers")
-const ac = document.getElementById("ac")
-const operator = document.querySelectorAll("operator")
-const equalSign = document.getElementById("equalSign")
-console.log(numbers)
-console.log(ac)
-console.log(operator)
-console.log(equalSign)
+const ac = document.getElementById("ac");
+const operator = document.querySelectorAll("#operator");
+const equalSign = document.getElementById("equalSign");
+const numbers = document.querySelectorAll("#number");
+const current = document.getElementById("current");
+const previous = document.getElementById("previous");
 
-numbers.forEach((n) => {
-    n.addEventListener("click", function () {
-      appendNumberFunction(n.innerText);
-    });
+numbers.forEach((num) => {
+  num.addEventListener("click", function () {
+    appendNumberFunction(num.innerText);
   });
-  
-  and 
-  
-  function appendNumberFunction(number) {
-    if (number === "." && current.innerText.includes(".")) {
+});
+
+ac.addEventListener("click", clear);
+
+operator.forEach((op) => {
+  op.addEventListener("click", function () {
+    if (current.innerText === "") {
       return;
     }
-    current.innerText += number;
+    chooseOperation(op.innerText);
+    previous.innerText = current.innerText;
+    current.innerText = "";
+  });
+});
+function appendNumberFunction(number) {
+  if (numbers === "." && current.innerText.includes(".")) {
+    return;
   }
+  current.innerText = current.innerText + number;
+}
+
+function clear() {
+  current.innerText = "";
+  previous.innerText = "";
+}
+
+function chooseOperation(operater) {
+  let answer;
+  const previousValue = previous.innerText;
+  const currentValue = current.innerText;
+  if (operater === "+") {
+    answer = currentValue + previousValue;
+  }
+  current.innerText = answer;
+}
