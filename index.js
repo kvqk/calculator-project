7b93adb2e7134378c5b45bcfb3c01c0426a7d161
Tyler <tyler@class-computers-MacBook-Air.local>
2022-05-08 14:00:28 -0700

    did operator js

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const equalSign = document.getElementById("equalSign");
 const numbers = document.querySelectorAll("#number");
 const current = document.getElementById("current");
 const previous = document.getElementById("previous");
-
+let choosenOperator;
 numbers.forEach((num) => {
   num.addEventListener("click", function () {
     appendNumberFunction(num.innerText);
   });
+});
+
+equalSign.addEventListener("click", function () {
+  chooseOperation();
 });
 
 ac.addEventListener("click", clear);
@@ -18,13 +22,13 @@ operator.forEach((op) => {
     if (current.innerText === "") {
       return;
     }
-    chooseOperation(op.innerText);
-    previous.innerText = current.innerText;
+    choosenOperator = op.innerText;
+    previous.innerText = `${current.innerText} ${op.innerText}`;
     current.innerText = "";
   });
 });
 function appendNumberFunction(number) {
-  if (numbers === "." && current.innerText.includes(".")) {
+  if (number === "." && current.innerText.includes(".")) {
     return;
   }
   current.innerText = current.innerText + number;
@@ -35,17 +39,20 @@ function clear() {
   previous.innerText = "";
 }
 
-function chooseOperation(operater) {
+function chooseOperation() {
   let answer;
-  const previousValue = previous.innerText;
-  const currentValue = current.innerText;
-  if (operater === "+") {
+  const previousValue = parseFloat(previous.innerText);
+  const currentValue = Number(current.innerText);
+  if (choosenOperator === "+") {
     answer = currentValue + previousValue;
+  } else if (choosenOperator === "-") {
+    answer = currentValue - previousValue;
+  } else if (choosenOperator === "x") {
+    answer = previousValue * currentValue;
+  } else if (choosenValue === "÷") {
+    answer = previousValue / currentValue;
   }
+
   current.innerText = answer;
-}
-let num1 
-let num2
-if(operator === "+"){
-  let
+  previous.innerText = "";
 }
