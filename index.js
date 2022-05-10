ed569ec2926c6c46f4b332f100c9647249953b40
Tyler <tyler@class-computers-MacBook-Air.local>
2022-05-09 17:04:09 -0700

    reset JS

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
-const ac = document.getElementById("ac");
-const operator = document.querySelectorAll("#operator");
-const equalSign = document.getElementById("equalSign");
-const numbers = document.querySelectorAll("#number");
-const current = document.getElementById("current");
-const previous = document.getElementById("previous");
-let choosenOperator;
-numbers.forEach((num) => {
-  num.addEventListener("click", function () {
-    appendNumberFunction(num.innerText);
-  });
-});
+//const all elements, current previous
 
-equalSign.addEventListener("click", function () {
-  chooseOperation();
-});
+//variable choosenOperator, apendNumberFunction
 
-ac.addEventListener("click", clear);
+// equak sign add event listener with choosenOperation
 
-operator.forEach((op) => {
-  op.addEventListener("click", function () {
-    if (current.innerText === "") {
-      return;
-    }
-    choosenOperator = op.innerText;
-    previous.innerText = `${current.innerText} ${op.innerText}`;
-    current.innerText = "";
-  });
-});
-function appendNumberFunction(number) {
-  if (number === "." && current.innerText.includes(".")) {
-    return;
-  }
-  current.innerText = current.innerText + number;
-}
+//clear add event listener ,clear
 
-function clear() {
-  current.innerText = "";
-  previous.innerText = "";
-}
+//operator for each
 
-function chooseOperation() {
-  let answer;
-  const previousValue = parseFloat(previous.innerText);
-  const currentValue = Number(current.innerText);
-  if (choosenOperator === "+") {
-    answer = currentValue + previousValue;
-  } else if (choosenOperator === "-") {
-    answer = currentValue - previousValue;
-  } else if (choosenOperator === "x") {
-    answer = previousValue * currentValue;
-  } else if (choosenValue === "÷") {
-    answer = previousValue / currentValue;
-  }
+//apendNumberFunction this is for the decimal
 
-  current.innerText = answer;
-  previous.innerText = "";
-}
+//clear function code
+
+// chooseOperation parse float Number,    if statements, else if statements
